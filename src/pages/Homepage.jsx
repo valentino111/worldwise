79826f5833dbb44be190709f4e104098bf33fbd2
valentino111 <valentino111@gmail.com>
2023-12-13 22:28:59 +0200

    208-209

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,15 @@
+import AppNav from "../components/AppNav";
 import PageNav from "../components/PageNav";
+import { Link } from "react-router-dom";
 
 function Homepage() {
   return (
     <div>
       <PageNav />
+      <AppNav />
       <h1>WorldWise</h1>
+
+      <Link to="/app">Go to the app</Link>
     </div>
   );
 }
